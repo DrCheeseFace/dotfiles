72ba875e3394ad56824877aa731abfd18e0b593d
= <tharun1@hotmail.co.uk>
2024-01-14 01:09:09 +0000

    cleaned up

--- nvim/lua/drcheeseface/packer.lua
+++ nvim/lua/drcheeseface/packer.lua
@@ -6,23 +6,20 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
 	  use({ 'rose-pine/neovim', as = 'rose-pine', 
   		config = function()
-			vim.cmd("colorscheme rose-pine")
+	--		vim.cmd("colorscheme rose-pine")
 		end
 	})
-  end)
 
-  require('packer').startup(function(use)
 	  use({ 'pineapplegiant/spaceduck', as = 'spaceduck', 
   		config = function()
 			vim.cmd("colorscheme spaceduck")
 		end
 	})
 
-  end)
-  
+
   use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
@@ -30,33 +27,14 @@ return require('packer').startup(function(use)
   use 'rafamadriz/friendly-snippets'
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp'
-  use 'wbthomason/packer.nvim'
   use {'VonHeikemen/lsp-zero.nvim'}
-
-  local lsp_zero = require('lsp-zero')
-
-  lsp_zero.on_attach(function(client, bufnr)
-      -- see :help lsp-zero-keybindings
-      -- to learn the available actions
-      lsp_zero.default_keymaps({buffer = bufnr})
-  end)
-
-  -- see :help lsp-zero-guide:integrate-with-mason-nvim
-  -- to learn how to use mason.nvim with lsp-zero
-  require('mason').setup({})
-  require('mason-lspconfig').setup({
-      handlers = {
-          lsp_zero.default_setup,
-      }
-  })
-
-  use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
   use {"ThePrimeagen/harpoon", 
       branch = "harpoon2", 
       requires = {{"nvim-lua/plenary.nvim"}}}
@@ -77,5 +55,22 @@ return require('packer').startup(function(use)
   use 'vim-airline/vim-airline'
   use 'vim-airline/vim-airline-themes'
   use {'jinh0/eyeliner.nvim'}
+
+  local lsp_zero = require('lsp-zero')
+
+  lsp_zero.on_attach(function(client, bufnr)
+      -- see :help lsp-zero-keybindings
+      -- to learn the available actions
+      lsp_zero.default_keymaps({buffer = bufnr})
+  end)
+
+  -- see :help lsp-zero-guide:integrate-with-mason-nvim
+  -- to learn how to use mason.nvim with lsp-zero
+  require('mason').setup({})
+  require('mason-lspconfig').setup({
+      handlers = {
+          lsp_zero.default_setup,
+      }
+  })
 
 end)
