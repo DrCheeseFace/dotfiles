9c553972fb50d090d93e3c8323852974fa636c69
= <tharun1@hotmail.co.uk>
2024-01-14 05:19:02 +0000

    added bind to clear notifs

--- nvim/after/plugin/notify.lua
+++ nvim/after/plugin/notify.lua
@@ -1,3 +1,5 @@
 require('notify').setup({
-    background_colour = "#000000"
+    background_colour = "#000000",
 })
+--dismiss notifications
+vim.keymap.set("n", "<leader>cn", function() require('notify').dismiss({ silent = true, pending = true}) end)
