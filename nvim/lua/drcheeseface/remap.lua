1eb4eb081244d8c48f925996468d80aa8dab20e4
= <tharun1@hotmail.co.uk>
2024-03-02 14:45:31 +0000

    nicer formatting

--- nvim/lua/drcheeseface/remap.lua
+++ nvim/lua/drcheeseface/remap.lua
@@ -1,12 +1,3 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
 -- Function to toggle between horizontal and vertical splits
 function HoriSplit()
     local current_win = vim.api.nvim_get_current_win()
@@ -19,6 +10,15 @@ function HoriSplit()
     end
 end
 
--- Bind the function to a key
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 vim.api.nvim_set_keymap('n', '<leader>ts', ':lua HoriSplit()<CR>', { noremap = true, silent = true })
 
