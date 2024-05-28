1133326fd3e67339f17854215395df50ca078c90
= <tharun1@hotmail.co.uk>
2024-05-28 23:39:11 +0100

    feat: mapped jumping trouble diagnostics

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -1,6 +1,13 @@
 require("trouble").setup {
     auto_preview = false,
-    auto_fold = true,
     use_diagnostics_signs = true
 }
-vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+
+vim.keymap.set("n", "[d", function()
+    require("trouble").next({skip_groups = true, jump = true});
+end)
+
+vim.keymap.set("n", "]d", function()
+    require("trouble").previous({skip_groups = true, jump = true});
+end)
