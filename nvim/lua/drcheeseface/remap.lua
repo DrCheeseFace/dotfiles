9d913bf902c4565ffb9f2f4a89a73ec812ba5243
= <tharun1@hotmail.co.uk>
2024-05-28 23:39:31 +0100

    feat: mapped format buffer

--- nvim/lua/drcheeseface/remap.lua
+++ nvim/lua/drcheeseface/remap.lua
@@ -10,7 +10,6 @@ function HoriSplit()
     end
 end
 
-
 vim.g.mapleader = " "
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -18,6 +17,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.api.nvim_set_keymap('n', '<leader>ts', ':lua HoriSplit()<CR>', { noremap = true, silent = true })
-
