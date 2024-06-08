bcec6f6ba9952e0a58619173fd444a344fdb0ace
drcheeseface <tharun1@hotmail.co.uk>
2024-06-08 17:58:27 +0100

    fix: kinda cringe i had to change this but oh well. trouble fixed

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -2,12 +2,12 @@ require("trouble").setup {
     auto_preview = false,
     use_diagnostics_signs = true
 }
-vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xx","<cmd>Trouble diagnostics toggle filter.buf=0<cr>")
 
 vim.keymap.set("n", "[d", function()
     require("trouble").next({skip_groups = true, jump = true});
 end)
 
 vim.keymap.set("n", "]d", function()
-    require("trouble").previous({skip_groups = true, jump = true});
+    require("trouble").prev({skip_groups = true, jump = true});
 end)
