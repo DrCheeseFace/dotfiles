c1263fd967d1d65ccee739c7196eb4b3de73102a
drcheeseface <tharun1@hotmail.co.uk>
2024-06-25 00:52:30 +0100

    fix: now always pages (before quit when everything fit on one page)

--- bash/cht.sh
+++ bash/cht.sh
@@ -7,8 +7,8 @@ fi
 read -p "Enter Query: " query
 query=`echo $query | tr ' ' '+'`
 if grep -q $selected ~/.config/bash/tmux-cht-languages; then
-    tmux neww zsh -c "curl -s cht.sh/$selected/$query | batcat -p"
+    tmux neww zsh -c "curl -s cht.sh/$selected/$query | batcat -p --paging=always"
 else
-    tmux neww zsh -c "curl -s cht.sh/$selected~$query | batcat -p"
+    tmux neww zsh -c "curl -s cht.sh/$selected~$query | batcat -p --paging=always"
 fi
 
