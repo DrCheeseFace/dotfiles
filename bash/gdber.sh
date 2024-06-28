6299068d57acad5907ee6080903511ff2a02f4fe
drcheeseface <tharun1@hotmail.co.uk>
2024-06-28 04:06:05 +0100

    much more readable gbder.sh

--- bash/gdber.sh
+++ bash/gdber.sh
@@ -7,9 +7,17 @@
 string=$1
 
 if [ -z "$string" ]; then
-    echo "Usage: gdber.sh <path> <string>"
+    echo "Usage: gdber.sh <string>"
     exit 1
 fi
 
-grep -n -H -r "$string" . | sed 's/\.\///' | awk -F':' '{printf "break %s:%d\n", $1, $2}' > /tmp/gdber.gdb && cat /tmp/gdber.gdb
+grep -n -H -r "$string" . \
+    | sed 's/\.\///' \
+    | awk -F':' '{printf "break %s:%d\n", $1, $2}' > /tmp/gdber.gdb
+
+# Print message indicating breakpoints generation
+echo "\e[1;32mBREAKPOINTS GENERATED AT /tmp/gdber.gdb\e[0m"
+
+# Display contents of the generated breakpoints file
+cat /tmp/gdber.gdb
 
