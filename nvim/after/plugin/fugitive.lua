61a15b53d0c233301ee3a3499b661f33b2e42641
drcheeseface <tharun1@hotmail.co.uk>
2024-07-08 13:48:52 +0100

    le toggle fugitive

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,1 +1,11 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+-- toggle git fugitive
+vim.keymap.set("n", "<leader>gs",
+    function()
+        for _, win in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+            if vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(win), 'filetype') == 'fugitive' then
+                return
+                    vim.api.nvim_win_close(win, true)
+            end
+        end
+        vim.cmd('Git')
+    end)
