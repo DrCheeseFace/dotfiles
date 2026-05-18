8b42f4e94b8bc2e2319a05ffa43232835a164e2c
tharun11 <tharun1@hotmail.co.uk>
2026-05-18 22:50:59 +0100

    hyprland waybar

--- nvim/lua/drcheeseface/set.lua
+++ nvim/lua/drcheeseface/set.lua
@@ -28,7 +28,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 50
--- vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
 
---
+++ waybar/wifi-menu.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+networkmanager_dmenu -d wofi
