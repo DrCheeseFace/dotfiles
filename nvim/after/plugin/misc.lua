a208ad8be3a7e0e27109f4771261fe4692f58bb7
tharun11 <tharun1@hotmail.co.uk>
2026-05-22 11:54:20 +0100

    remove ansi command

--- nvim/after/plugin/misc.lua
+++ nvim/after/plugin/misc.lua
@@ -1,3 +1,7 @@
 require("mini.surround").setup {}
 require("fidget").setup({})
 require("nvim-web-devicons").setup({})
+
+vim.api.nvim_create_user_command('RemoveAnsi', function()
+  vim.cmd([[%s/\v\e\[[0-9;]*[mK]//g]])
+end, {})
